e6bc88d420dd1cc2eb7c48ceda95a585253b7203
Jonathan Dawson <103955945+jondawson917@users.noreply.github.com>
2022-12-12 14:28:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from forms import AddTripForm, EditTripForm, AddPassengerForm, EditPassengerForm
 from sqlalchemy.exc import IntegrityError
 from API_data import *
 import os
-
+from seed import seed
 
 app = Flask(__name__)
 
@@ -22,7 +22,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 with app.app_context():
-        db.create_all()
+    db.create_all()
+    seed()
 
 
 
